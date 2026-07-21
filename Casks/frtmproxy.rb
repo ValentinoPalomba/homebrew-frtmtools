455cb19448b739eef20d08fe99013be85bae3a50
Valentino Palomba <valentino.palomba@intesasanpaolo.com>
2026-07-21 13:18:53 +0200

    frtmproxy 1.8.0

--- Casks/frtmproxy.rb
+++ Casks/frtmproxy.rb
@@ -1,8 +1,8 @@
 cask "frtmproxy" do
-  version "1.6.0"
-  sha256 "256a00c1d40b15d27fa6a30c6011b351a69bc99cf0ec0c82bc8b1d103d84f818"
+  version "1.8.0"
+  sha256 "e0b2bb13d37b4b38b15510e4ec4540aebfc6d97a57dc1846a305c1bfec0816a5"
 
-  url "https://github.com/ValentinoPalomba/FRTMProxy/releases/download/v.1.6.0/FRTMProxy-1.6.0.zip"
+  url "https://github.com/ValentinoPalomba/FRTMProxy/releases/download/v.1.8.0/FRTMProxy-1.8.0.zip"
   name "FRTMProxy"
   desc "Observe, understand, and shape HTTP/S traffic in real time"
   homepage "https://github.com/ValentinoPalomba/FRTMProxy"
